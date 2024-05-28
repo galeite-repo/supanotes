269d4bff66dc42f8a640be73b8370ab37439c623
Gabriel Leite <galeite@ibm.com>
2024-05-28 10:45:54 -0300

    add

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -12,6 +12,7 @@ export class AuthService {
   constructor() {
     this._supabase.auth.onAuthStateChange((session) => {
       console.log(session);
+      console.log(session);
     });
   }
   session() {
